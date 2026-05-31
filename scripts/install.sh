bb6f2774d6c11c3e19c8932eaca4e4243268e0d1
Dmitrii Matrokhin <matrokhin.d@gmail.com>
2026-05-31 17:35:37 +0200

    Make install/update scripts run from any directory and skip building tests

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/sh
 
+set -e
+
+REPO_DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
+cd "$REPO_DIR"
+
 cmake --preset release
-cmake --build ./build/Release
+cmake --build ./build/Release --target prock
 
 sudo cmake --install ./build/Release
 sudo cp prock.desktop /usr/share/applications/
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/sh
 
+set -e
+
+REPO_DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
+cd "$REPO_DIR"
+
 git pull
 
-cmake --build ./build/Release
+cmake --build ./build/Release --target prock
 sudo cmake --install ./build/Release
